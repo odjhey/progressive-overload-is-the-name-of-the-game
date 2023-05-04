759fd24c87cdced678d07d3b2264b9ae49c534d3
odjhey <odjhey@gmail.com>
2023-05-04 09:22:24 +0800

    docs: update about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,10 @@
 export default function About() {
+
     return (
-        <div>About</ div >
+        <>
+            <div>About</ div >
+            <div>Goals</div>
+            <div>Track progress of overloads, may be increase in lifted weight overtime, or increase in reps/sets</div>
+        </>
     );
 }
