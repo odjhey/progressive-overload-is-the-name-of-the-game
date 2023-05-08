619183c523e52a4304d19f649a992c156417bd66
odjhey <odjhey@gmail.com>
2023-05-08 09:44:36 +0800

    feat: add useTags hook

---
+++ src/hooks/useTags.ts
@@ -0,0 +1,85 @@
+import localforage from "localforage";
+import { useEffect, useState } from "react";
+
+type Tag = {
+  name: string;
+  liftName: string;
+};
+
+// TODO: add version support
+// TODO: fix typescript
+const lskey = "TAGSlksajdf";
+export const useTags = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error>();
+  const [data, setData] = useState<Tag[]>([]);
+
+  const reloadItem = () => {
+    return localforage
+      .getItem(lskey)
+      .then((d) => {
+        const defaultTags: Tag[] = [];
+        const typedD = d as { tags: Tag[] };
+        if (typedD && typedD.tags) {
+          return typedD.tags;
+        }
+        return defaultTags;
+      })
+      .then((d) => {
+        setData(d);
+      });
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    reloadItem()
+      .catch((e) => {
+        setError(e);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  const saveTags = (tags: Tag[]) => {
+    setLoading(true);
+    return localforage
+      .setItem(lskey, { tags })
+      .catch((e) => {
+        setError(e);
+      })
+      .then(() => {
+        // reload
+        return reloadItem();
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const appendTag = (
+    tag: Tag,
+    options?: {
+      onSuccess: (tag: Tag) => void;
+    }
+  ) => {
+    setLoading(true);
+    return localforage
+      .setItem(lskey, { tags: [...data, tag] })
+      .catch((e) => {
+        setError(e);
+      })
+      .then((_) => {
+        // reload
+        reloadItem();
+        if (options && typeof options.onSuccess === "function") {
+          options.onSuccess(tag);
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return { loading, data, error, saveTags, appendTag };
+};
