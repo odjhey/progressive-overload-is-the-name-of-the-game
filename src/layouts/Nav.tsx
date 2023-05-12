2aadbe78f0db0866f390f82108bf0cbbf5fa72b1
odjhey <odjhey@gmail.com>
2023-05-12 14:27:06 +0800

    refactor: remove toast stuff

--- src/layouts/Nav.tsx
+++ src/layouts/Nav.tsx
@@ -1,55 +1,9 @@
 import {
   PropsWithChildren,
-  createContext,
-  useCallback,
-  useEffect,
-  useState,
 } from 'react'
 import { Link } from 'react-router-dom'
-import * as Toast from '@radix-ui/react-toast'
 
-export const ToastContext = createContext()
-
-const useToast = () => {}
-
-const ToastWrapper = ({ children }: PropsWithChildren) => {
-  const [messages, setMessages] = useState<{ message: string }[]>([])
-
-  const toast = useCallback(
-    (message: string) => {
-      setMessages((prev) => [...prev, { message }])
-    },
-    [setMessages]
-  )
-
-  useEffect(() => {
-    if (messages.length > 0) {
-      const timer = setTimeout(() => {
-        setMessages((prev) => prev.slice(1))
-      }, 2000)
-      return () => clearTimeout(timer)
-    }
-  }, [messages])
-
-  return (
-    <ToastContext.Provider value={{ toast }}>
-      <Toast.Provider swipeDirection="right">
-        {children}
-        {messages.map((m) => {
-          return (
-            <Toast.Root open={true}>
-              <Toast.Title>{m.message}</Toast.Title>
-              <Toast.Description asChild>{m.message}</Toast.Description>
-            </Toast.Root>
-          )
-        })}
-        <Toast.Viewport className="fixed bottom-5 flex flex-col w-full items-center"></Toast.Viewport>
-      </Toast.Provider>
-    </ToastContext.Provider>
-  )
-}
-
-export default function Nav({ children }: PropsWithChildren) {
+export default function Nav(_props: PropsWithChildren) {
   return (
     <>
       <div>
@@ -77,7 +31,6 @@ export default function Nav({ children }: PropsWithChildren) {
         </nav>
       </div>
 
-      <ToastWrapper>{children}</ToastWrapper>
     </>
   )
 }
