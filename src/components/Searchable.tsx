f837bbce7407a2ebe328d525cb7e3cda4eaa0f37
odjhey <odjhey@gmail.com>
2023-05-13 10:43:13 +0800

    feat: add tag search

--- src/components/Searchable.tsx
+++ src/components/Searchable.tsx
@@ -4,14 +4,16 @@ export const Searchable = ({
   searchTerm,
   children,
   setSearchTerm,
+  placeholder,
 }: PropsWithChildren<{
   searchTerm: string
   setSearchTerm: (searchTerm: string) => void
+  placeholder?: string
 }>) => {
   return (
     <>
       <input
-        placeholder="search"
+        placeholder={placeholder || 'Search'}
         defaultValue={searchTerm}
         className="input input-sm"
         onChange={(e) => setSearchTerm(e.target.value)}
